b7a26a3365d727e9892f7d0d120c541b9411c7c9
SunnyTany <46300377+SunnyTany@users.noreply.github.com>
2026-05-01 07:53:15 +0200

    refactor main

--- src/app/Main.java
+++ src/app/Main.java
@@ -5,14 +5,13 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Create a specific account object with an initial balance
         Account myAccount = new Account(1000.00);
         Scanner scanner = new Scanner(System.in);
 
         System.out.printf("Balance is USD %.2f.%n" +
                 "Enter purchase amount, USD: ", myAccount.getBalance());
 
-        // Input validation (not a number)
+        // 1. Input validation (not a number)
         if (!scanner.hasNextDouble()) {
             System.out.println("Error: Invalid input. Please enter a number.");
             return;
@@ -20,14 +19,16 @@ public class Main {
 
         double amount = scanner.nextDouble();
 
+        // 2. In the try block, only the method that throws the exception
         try {
-            // We are trying to withdraw money
             myAccount.withdraw(amount);
-            System.out.println("Funds are OK. Purchase paid.");
-            System.out.printf("Balance is USD %.2f%n", myAccount.getBalance());
         } catch (FundsException ex) {
-            // We are processing the lack of funds
             System.out.println(ex.getMessage());
+            return; // Aborting execution because the transaction failed.
         }
+
+        // 3. Logic of successful completion
+        System.out.println("Funds are OK. Purchase paid.");
+        System.out.printf("Balance is USD %.2f%n", myAccount.getBalance());
     }
 }
